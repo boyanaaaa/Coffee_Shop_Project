697c467788b5e5266f8812654b1a74e0bba443a1
Boyana Dimitrova <125132910+boyanaaaa@users.noreply.github.com>
2023-02-23 09:27:59 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const heart = document.getElementById('heartIcon');
-
-heart.addEventListener("click", () => {
-    heart.classList.remove("bx bx-heart");
-    heart.classList.add("bx bxs-heart");
-})
